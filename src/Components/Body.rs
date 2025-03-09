807d25430c0533f385e133df7ef082c10d84783f
YuruDeveloper <vkgkvk8@gmail.com>
2025-03-09 15:51:14 +0900

    Childern Chage

--- src/Components/Body.rs
+++ src/Components/Body.rs
@@ -1,8 +1,15 @@
 use yew::prelude::*;
 
+#[derive(Properties,PartialEq)]
+pub struct Props{
+    pub Children : Children
+}
+
 #[function_component]
-pub fn Body() -> Html{
+pub fn Body(Props : &Props) -> Html{
     html!{
-        <main class="container mx-auto max-w-6xl px-4 py-8"/>
+        <main class="container mx-auto max-w-6xl px-4 py-8">
+            {for Props.Children.iter()}
+        </main>
     }
 }
