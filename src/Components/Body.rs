36b8834a00894c05904104eac9b3eccb8687205d
YuruDeveloper <vkgkvk8@gmail.com>
2025-03-09 16:16:33 +0900

    childern change2

--- src/Components/Body.rs
+++ src/Components/Body.rs
@@ -2,14 +2,14 @@ use yew::prelude::*;
 
 #[derive(Properties,PartialEq)]
 pub struct Props{
-    pub Children : Children
+    pub Children : Html
 }
 
 #[function_component]
 pub fn Body(Props : &Props) -> Html{
     html!{
         <main class="container mx-auto max-w-6xl px-4 py-8">
-            {for Props.Children.iter()}
+            { Props.Children.clone()}
         </main>
     }
 }
